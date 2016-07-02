bae9335d1297543cd80287f32eaefb91ae810699
mastermark5 <mastermark51200@gmail.com>
2016-07-02 00:09:13 -0400

    Setting up project, etc

---
+++ src/com/markaldrich/chess/Main.java
@@ -0,0 +1,7 @@
+package com.markaldrich.chess;
+
+/**
+ * Created by maste on 7/2/2016.
+ */
+public class Main {
+}
---
+++ src/com/markaldrich/chess/chessGame/ChessGame.java
@@ -0,0 +1,7 @@
+package com.markaldrich.chess.chessGame;
+
+/**
+ * Created by maste on 7/2/2016.
+ */
+public class ChessGame {
+}
